06776bdc185c40d3f4596493c63dfc296d70ddef
Ty Garber <tyler.garber@dfw.wa.gov>
2023-04-19 16:03:58 -0700

    intial commit

---
+++ populate_database.R
@@ -0,0 +1,86 @@
+##########################################
+#### this script pulls all the csvs   ####
+#### from the RMIS website, appends   ####
+#### to database. Ty Garber 4/13/2023 ####
+#### if a sqlite database doesn't     #### 
+#### exist it will create one.        ####
+##########################################
+
+
+# libraries
+library(RCurl)
+library(purrr)
+library(stringr)
+library(DBI)
+library(readr)
+library(rvest)
+library(dplyr)
+
+# link to rmis ftp site
+rmis_url <- "https://www.rmpc.org/pub/data/"
+
+# fetch names, last updated times from website
+filenames <- getURL(rmis_url) 
+
+# read html table into R
+files <- filenames %>% read_html() %>% html_table()
+
+files_df <- files[[1]] %>%
+  janitor::clean_names() %>%
+  select(name, last_modified) %>%
+  filter(
+    str_detect(name, '.csv'),
+    str_detect(name, 'RC') |
+      str_detect(name, 'RL041_ALL_FULLSET.csv') |
+      str_detect(name, 'LC041_ALL_FULLSET.csv') 
+  ) %>%
+  rowwise() %>%
+  mutate(
+    file_id = uuid::UUIDgenerate(),
+    last_modified = as.character(as.Date(last_modified))
+  )
+
+con <- dbConnect(RSQLite::SQLite(), "rmis.db")
+
+# don't want to commit this to memory it's multiple gb
+# populate the database
+files_df %>%
+  pull(name) %>%
+  imap(~ {
+    if (.x == 'LC041_ALL_FULLSET.csv') {
+      dbWriteTable(
+        con,
+        'locations',
+        read_csv(paste0(rmis_url, .x), col_types = cols(.default = "c"))
+        ,
+        overwrite = TRUE
+      )
+    } else if (str_detect(.x, 'RC')) {
+      dbWriteTable(
+        con,
+        'recoveries',
+        read_csv(paste0(rmis_url, .x), col_types = cols(.default = "c")) %>%
+          mutate(file_id = files_df$file_id[.y])
+        ,
+        append = TRUE
+      )
+    } else if (.x == 'RL041_ALL_FULLSET.csv'){
+      dbWriteTable(
+        con,
+        'releases',
+        read_csv(paste0(rmis_url, .x), col_types = cols(.default = "c"))
+        ,
+        overwrite = TRUE
+      )
+    }
+  }
+)
+# save a file log of updates to rmis csvs
+dbWriteTable(
+  con,
+  'file_log',
+  files_df,
+  overwrite = TRUE
+)
+
+DBI::dbDisconnect(con)
---
+++ update_database.R
@@ -0,0 +1,167 @@
+##########################################
+#### this script pulls all the csvs   ####
+#### from the RMIS website, appends   ####
+#### to database. Ty Garber 4/13/2023 ####
+#### if a sqlite database doesn't     #### 
+#### exist it will create one.        ####
+##########################################
+
+
+# libraries
+library(RCurl)
+library(purrr)
+library(stringr)
+library(DBI)
+library(readr)
+library(rvest)
+library(dplyr)
+
+# link to rmis ftp site
+rmis_url <- "https://www.rmpc.org/pub/data/"
+
+# fetch names, last updated times from website
+filenames <- getURL(rmis_url) 
+
+# read html table into R
+files <- filenames %>% read_html() %>% html_table()
+
+files_df <- files[[1]] %>%
+  janitor::clean_names() %>%
+  select(name, last_modified) %>%
+  filter(
+    str_detect(name, '.csv'),
+    str_detect(name, 'RC') |
+      str_detect(name, 'RL041_ALL_FULLSET.csv') |
+      str_detect(name, 'LC041_ALL_FULLSET.csv') 
+  ) %>% 
+  mutate(
+    last_modified = as.character(as.Date(last_modified))
+  )
+
+con <- dbConnect(RSQLite::SQLite(), "rmis.db")
+
+file_log <- dbReadTable(con, 'file_log') %>%
+  as_tibble()
+
+# check if files have been updated
+update_files <- files_df %>%
+  inner_join(file_log, by='name', suffix=c('_rmis', '_db')) %>%
+  filter(
+    as.Date(last_modified_rmis) > (as.Date(last_modified_db))
+  )
+
+
+updater <- function (file_name, uuid, last_modified){
+  
+  if(length(file_name) == 0){
+    return(TRUE)
+  }
+  
+  if(file_name == 'RL041_ALL_FULLSET.csv') {
+    
+    print(glue::glue('Updating releases dataset {file_name} - {uuid} - {last_modified}'))
+    
+    # replace releases table
+     dbWriteTable(
+       con,
+       'releases',
+       read_csv(paste0(rmis_url, file_name), col_types = cols(.default = "c"))
+       ,
+       overwrite = TRUE
+     )
+
+     update <- DBI::dbSendQuery(con, glue::glue(
+       "UPDATE file_log
+        SET
+           last_modified = '{last_modified}',
+           file_id = '{uuid::UUIDgenerate()}'
+        WHERE
+           file_id = '{uuid}'
+       "
+     ))
+     DBI::dbClearResult(update)
+
+
+    
+    
+  } else if (file_name == 'LC041_ALL_FULLSET.csv'){
+    print(glue::glue('Updating locations dataset {file_name} - {uuid} - {last_modified}'))
+    # replace releases table
+    dbWriteTable(
+      con,
+      'locations',
+      read_csv(paste0(rmis_url, file_name), col_types = cols(.default = "c"))
+      ,
+      overwrite = TRUE
+    )
+    
+    update <- DBI::dbSendQuery(con, glue::glue(
+      "UPDATE file_log
+        SET
+           last_modified = '{last_modified}',
+           file_id = '{uuid::UUIDgenerate()}'
+        WHERE
+           file_id = '{uuid}'
+       "
+    ))
+    DBI::dbClearResult(update)
+    
+    
+  } else {
+    print(glue::glue('Updating recovery dataset {file_name} - {uuid} - {last_modified}'))
+    
+    new_uuid = uuid::UUIDgenerate()
+    # replace releases table
+    dbWriteTable(
+      con,
+      'recoveries',
+      read_csv(paste0(rmis_url, file_name), col_types = cols(.default = "c")) %>%
+        mutate(file_id = new_uuid)
+      ,
+      append = TRUE
+    )
+    
+    print('Updating ')
+    update <- DBI::dbSendQuery(con, glue::glue(
+      "UPDATE file_log
+        SET
+           last_modified = '{last_modified}',
+           file_id = '{new_uuid}'
+        WHERE
+           file_id = '{uuid}';
+       "
+    ))
+    DBI::dbClearResult(update)
+    
+    delete <-
+      DBI::dbSendQuery(con, glue::glue(
+        "DELETE FROM recoveries WHERE file_id = '{uuid}';"
+      ))
+    DBI::dbClearResult(delete)
+  }
+  
+  TRUE
+}
+
+#bc3a6698-5f02-41ae-ba3f-e16e9699d8c6
+
+update_files %>%
+  rowwise() %>%
+  mutate(
+    success = updater(name, file_id, last_modified_rmis)
+  )
+
+DBI::dbDisconnect(con)
+
+
+# check for new files
+new_files <- files_df %>%
+  left_join(file_log, by='name', suffix=c('_rmis', '_db')) %>%
+  filter(
+    is.na(file_id)
+  )
+
+
+
+
+
